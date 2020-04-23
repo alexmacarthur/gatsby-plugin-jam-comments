3289fda0fb6af714923aa32b7dde202a94438c86
Alex MacArthur <alex.macarthur@daveramsey.com>
2020-04-22 21:42:50 -0500

    successfully sending comments to server.

--- ui/CommentBox/index.js
+++ ui/CommentBox/index.js
@@ -10,19 +10,38 @@ export default () => {
             return !!el.value;
         });
 
+        let formData = withValues.map(input => {
+            return {
+                name: input.name,
+                value: input.value
+            }
+        });
+
+        let mutationParams = withValues.reduce((totalString, input, index) => {
+            let string = totalString + `${input.name}: "${input.value}"`;
+            return index + 1 < withValues.length ? string + ', ' : string;
+        }, "");
+
+        console.log(mutationParams);
+
         console.log(process.env.GATSBY_JAM_COMMENTS_API_KEY);
 
         const response = await fetch('http://localhost:4000/graphql', {
             method: 'POST',
             headers: { 
                 'Content-Type': 'application/json',
-                'x-api-key': process.env.GATSBY_JAM_COMMENTS_API_KEY,
-                'x-domain': 'hello2.com'
+                // 'x-api-key': process.env.GATSBY_JAM_COMMENTS_API_KEY,
+                'x-api-key': "7T0CRJM-0N34CT8-MQEX3CP-JHGQ6R2",
+                'x-domain': 'mysitename.com'
             },
             body: JSON.stringify({ query: `
                 mutation {
-                    createComment(content: "new c33onddfsdstent!", userId: null, siteId: 3) {
+                    createComment(${mutationParams}) {
                         createdAt
+                        name
+                        twitterHandle
+                        emailAddress
+                        content
                     }
                 }` 
             }),
@@ -35,7 +54,7 @@ export default () => {
         <form onSubmit={submitComment} ref={formRef} className={styles.box}>
             <label className={styles.label}>
                 Comment
-                <textarea name="comment" required={true}></textarea>
+                <textarea name="content" required={true}></textarea>
             </label>
             <label className={styles.label}>
                 Name
@@ -43,11 +62,11 @@ export default () => {
             </label>
             <label className={styles.label}>
                 Email Address
-                <input type="email" name="email" />
+                <input type="email" name="emailAddress" />
             </label>
             <label className={styles.label}>
                 Twitter Handle
-                <input type="text" name="twitter_handle" />
+                <input type="text" name="twitterHandle" />
             </label>
 
             <button>Submit</button>
