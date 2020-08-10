e44f5673e472002dcb3291b62bb9e02f3e389564
Alex MacArthur <alex.macarthur@daveramsey.com>
2020-08-09 22:00:34 -0500

    Adjust styles to make more flexible.

--- src/ui/Message/index.js
+++ src/ui/Message/index.js
@@ -3,5 +3,11 @@ import "./style.scss"
 
 export default ({ children, isSuccessful = false }) => {
   const cssClass = isSuccessful ? "jc-Message--success" : "jc-Message--error"
-  return <div className={`jc-Message ${cssClass}`}>{children}</div>
+  return (
+    <div className={`jc-Message ${cssClass}`}>
+      <p>
+        {children}
+      </p>
+    </div>
+  );
 }
