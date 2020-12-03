17c4f627c0db9d14735eaf609f79d22cb601a4bf
Alex MacArthur <alex@macarthur.me>
2020-12-02 22:27:40 -0600

    Slim down bundle size a bit.

--- src/ui/CommentBox/questClient.js
+++ src/ui/CommentBox/questClient.js
@@ -1,9 +1,10 @@
 import { QuestClient } from "graphql-quest";
-import { getServiceEndpoint } from "jam-comments-utilities/shared";
+import getServiceEndpoint from "jam-comments-utilities/shared/getServiceEndpoint";
 
 const client = QuestClient({
-  endpoint: `${process.env.JAM_COMMENTS_SERVICE_ENDPOINT ||
-    getServiceEndpoint()}/graphql`,
+  endpoint: `${getServiceEndpoint(
+    process.env.JAM_COMMENTS_SERVICE_ENDPOINT
+  )}/graphql`,
   headers: {
     "x-api-key": process.env.GATSBY_JAM_COMMENTS_API_KEY
   }
